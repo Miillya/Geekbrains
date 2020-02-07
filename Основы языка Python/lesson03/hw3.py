bd60ab8576eff2477578668ff7bbc408e229b82f
busiko7 <tankov.aa@yandex.ru>
2020-02-07 11:26:06 +0300

    Основы языка Python. Домашнее задание к уроку №3. Добавление варианта №2.

--- Основы языка Python/lesson03/hw3.py
+++ Основы языка Python/lesson03/hw3.py
@@ -11,14 +11,14 @@ import random
 print('Игра - "Угадай число"')
 print('Загадайте число от 1 до 100')
 
+# Вариант 1
 count = 0
 START = 1
 END = 100
 answer = ''
-
 number = random.randint(START, END)
 
-while not answer == "=":
+while answer != '=':
     print(f'Вы загадали число - {number}?')
     print(f'Введите знак "=" - если компьютер угадал Ваше число, ">" - если число больше, "<" - если число меньше')
     answer = input('Введите Ваш знак: ')
@@ -37,7 +37,32 @@ while not answer == "=":
         END = number - 1
         number = random.randint(START, END)
     else:
-        print('Введите корректное значение')
+        print('Введите корректное значение - ">", "<" или "="')
 else:
     print(f'Вы вышли из заданного диапазона чисел')
     exit()
+
+# Вариант 2
+# count = 0
+# START = 0
+# END = 101
+#
+# number = int((END - START) / 2)
+# answer = None
+# while answer != '=':
+#     count += 1
+#     print(f'Вы загадали число - {number}?')
+#     print(f'Введите знак "=" - если компьютер угадал Ваше число, ">" - если число больше, "<" - если число меньше')
+#     answer = input('Введите Ваш знак: ')
+#     if answer == '<':
+#         END = number
+#         number -= int((END - START) / 2)
+#     elif answer == '>':
+#         START = number
+#         number += int((END - START) / 2)
+#     elif answer == '=':
+#         continue
+#     else:
+#         print('Введите корректное значение - ">", "<" или "="')
+# else:
+#     print(f'Компьютер угадал число с {count}-й попытки!')
