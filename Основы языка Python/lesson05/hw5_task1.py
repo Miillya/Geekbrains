d10b0a19759698130a690d1f2f53ab35ff348b84
busiko7 <tankov.aa@yandex.ru>
2020-02-16 21:39:56 +0300

    Основы языка Python. Домашнее задание к уроку №5.

---
+++ Основы языка Python/lesson05/hw5_task1.py
@@ -0,0 +1,32 @@
+# 1. Создайте модуль (модуль - программа на Python, т.е. файл с расширением .py). В нем создайте функцию создающую
+# директории от dir_1 до dir_9 в папке из которой запущен данный код. Затем создайте вторую функцию удаляющую эти
+# папки. Проверьте работу функций в этом же модуле.
+import os
+
+name = 'dir'
+
+
+# Функция создания директории:
+def create_dir():
+    i = 1
+    while i < 10:
+        dir_name = os.path.join(os.getcwd(), '{}_{}'.format(name, i))
+        i += 1
+        try:
+            os.mkdir(dir_name)
+            print('Директория', dir_name, 'создана!')
+        except FileExistsError:
+            print('Директория', dir_name, 'уже существует')
+
+
+# Функция удаления директории:
+def delete_dir():
+    i = 1
+    while i < 10:
+        dir_name = os.path.join(os.getcwd(), '{}_{}'.format(name, i))
+        i += 1
+        try:
+            os.rmdir(dir_name)
+            print('Директория', dir_name, 'удалена!')
+        except FileNotFoundError:
+            print('Директории', dir_name, 'не существует')
