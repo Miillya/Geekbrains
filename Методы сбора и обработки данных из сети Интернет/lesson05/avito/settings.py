092a5397333469fb06c084ca98abfd58dc42d29a
busiko7 <tankov.aa@yandex.ru>
2020-03-31 14:47:13 +0300

    Методы сбора и обработки данных из сети Интернет. Урок 5. Scrapy Практическое задание. v2

--- Методы сбора и обработки данных из сети Интернет/lesson05/avito/settings.py
+++ Методы сбора и обработки данных из сети Интернет/lesson05/avito/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for blogparse project
+# Scrapy settings for avito project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,10 +9,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'blogparse'
+BOT_NAME = 'avito'
 
-SPIDER_MODULES = ['blogparse.spiders']
-NEWSPIDER_MODULE = 'blogparse.spiders'
+SPIDER_MODULES = ['avito.spiders']
+NEWSPIDER_MODULE = 'avito.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
@@ -25,13 +25,15 @@ LOG_ENABLED = True
 
 LOG_LEVEL = 'DEBUG'
 
+IMAGES_STORE = 'images'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -51,13 +53,13 @@ DOWNLOAD_DELAY = 1
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'blogparse.middlewares.BlogparseSpiderMiddleware': 543,
+#    'avito.middlewares.AvitoSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'blogparse.middlewares.BlogparseDownloaderMiddleware': 543,
+#    'avito.middlewares.AvitoDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -69,7 +71,7 @@ DOWNLOAD_DELAY = 1
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'blogparse.pipelines.BlogparsePipeline': 100,
+   'avito.pipelines.AvitoPipeline': 100,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
