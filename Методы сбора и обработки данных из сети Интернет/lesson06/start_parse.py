31f47f3ed1bbf9cf5cec1ef20c29bfe935ec11bd
busiko7 <tankov.aa@yandex.ru>
2020-04-09 14:40:11 +0300

    Методы сбора и обработки данных из сети Интернет. Правки.

--- Методы сбора и обработки данных из сети Интернет/lesson06/start_parse.py
+++ Методы сбора и обработки данных из сети Интернет/lesson06/start_parse.py
@@ -1,11 +1,17 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from avito.spiders.avito import AvitoSpider
-from avito import settings
+from blogparse.spiders.instagram import InstagramSpider
+from blogparse import settings
+
+env_path = Path(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
 
 if __name__ == '__main__':
     craw_settings = Settings()
     craw_settings.setmodule(settings)
     crawler_proc = CrawlerProcess(settings=craw_settings)
-    crawler_proc.crawl(AvitoSpider)
+    crawler_proc.crawl(InstagramSpider, logpass=(os.getenv('LOGIN'), os.getenv('PSWD')))
     crawler_proc.start()
