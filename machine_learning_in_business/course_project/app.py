ac9969c2d25425f8a14f3e78c786799e2dcddae2
busiko7 <tankov.aa@yandex.ru>
2020-11-26 15:52:58 +0300

    Машинное обучение в бизнесе. Курсовой проект. 1.5.4 - Readme, port, requirements

--- machine_learning_in_business/course_project/app.py
+++ machine_learning_in_business/course_project/app.py
@@ -3,7 +3,7 @@ from pycaret.classification import *
 import pandas as pd
 import pickle
 import numpy as np
-import config.py
+import config
 
 app = Flask(__name__)
 
@@ -23,8 +23,8 @@ def home():
 def predict():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = predict_model(model, data=data_unseen, round = 0)
+    data_unseen = pd.DataFrame([final], columns=cols)
+    prediction = predict_model(model, data=data_unseen, round=0)
     prediction = int(prediction.Label[0])
     return render_template('home.html', pred='Expected Bill will be {}'.format(prediction))
 
